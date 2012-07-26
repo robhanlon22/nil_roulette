b56d5bf1873f3bf83461c54ea1696d94d6786314
Rob Hanlon <rob@mediapiston.com>
2012-07-26 12:36:09 -0700

    Prevent infinite recursion.

--- lib/nil_roulette.rb
+++ lib/nil_roulette.rb
@@ -2,6 +2,7 @@ require "nil_roulette/version"
 
 module NilRoulette
   def nil.method_missing(sym, *args, &block)
-    ObjectSpace.each_object.to_a.shuffle.find { |o| o.respond_to?(sym) }.send(sym, *args, &block)
+    object = ObjectSpace.each_object.to_a.shuffle.find { |o| o.respond_to?(sym) }
+    object.send(sym, *args, &block) unless object.nil?
   end
 end
