14fe190b7ab10d7f78684e4f68362467a2557a05
kellydunn <defaultstring@gmail.com>
2012-07-26 20:59:36 -0700

    [spec] Adding some unit tests

--- spec/unit/nil_roulette_spec.rb
+++ spec/unit/nil_roulette_spec.rb
@@ -1,7 +1,19 @@
 require "spec_helper"
+require "nil_roulette"
+require "digest"
 
 describe "Nilroulette" do
-  it "should just fuckin call the shit"
-  it "should maybe call that fuckin shit"
-  it "really shouldn't call that shit"
+  it "should just fuckin call that shit" do
+    nil.send(Digest::SHA1.hexdigest("#{Time.now.utc.to_i * rand(9001)}"))
+  end
+
+  it "shouldn't be a little bitch" do
+    begin
+      nil.send(Digest::SHA1.hexdigest("#{Time.now.utc.to_i * rand(9001)}"))
+    rescue Exception => e
+      error = e
+    ensure
+      error.should be_nil
+    end
+  end
 end
